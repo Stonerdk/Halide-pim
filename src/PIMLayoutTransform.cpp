0f2adc5dbd614b91bd8ee686764e93b8721a7203
stonerdk <stkd97@gmail.com>
2023-08-21 18:40:56 +0000

    Complete LT

--- src/PIMLayoutTransform.cpp
+++ src/PIMLayoutTransform.cpp
@@ -28,6 +28,7 @@ using std::string;
 using std::vector;
 using std::set;
 using std::pair;
+using std::stack;
 using std::reverse;
 
 class PIMLayoutTransform : public IRMutator {
@@ -116,47 +117,94 @@ public:
 private:
     size_t kernel_idx = 0;
     using IRMutator::visit;
+    Scope<> reduction_scope;
+    Scope<map<string, Box>> bounds_scope;
+    set<string> shared_memory_buffer;
+
+    Stmt visit(const ProducerConsumer* op) override {
+        reduction_scope.push(op->name);
+        Stmt stmt = IRMutator::visit(op);
+        reduction_scope.pop(op->name);
+        return stmt;
+    }
+
+    string get_kernel_name() {
+        return "kernel_" + std::to_string(kernel_idx);
+    }
 
     Stmt visit(const For* loop) override {
+        if (ends_with(loop->name, ".__thread_id_x")) {
+            stack<pair<string, Expr>> lets;
+            map<string, Box> bounds = bounds_scope.get(get_kernel_name());
+            for (auto it = bounds.begin(); it != bounds.end(); it++) {
+                Expr stride = 1;
+                for (size_t i = 0; i < it->second.size(); i++) {
+                    if (shared_memory_buffer.find(it->first) != shared_memory_buffer.end()) {
+                        lets.push({ it->first + ".min.pim." + std::to_string(i), 
+                            Variable::make(Int(32), it->first + ".min." + std::to_string(i)) });
+                        lets.push({ it->first + ".extent.pim." + std::to_string(i), 
+                            Variable::make(Int(32), it->first + ".extent." + std::to_string(i)) });
+                        lets.push({ it->first + ".stride.pim." + std::to_string(i), 
+                            Variable::make(Int(32), it->first + ".stride." + std::to_string(i)) });
+                    } else {
+                        lets.push({it->first + ".min.pim." + std::to_string(i), it->second[i].min});
+                        Expr extent = it->second[i].max - it->second[i].min + 1;
+                        lets.push({ it->first + ".extent.pim." + std::to_string(i), extent });
+                        lets.push({ it->first + ".stride.pim." + std::to_string(i), stride });
+                        stride *= extent;
+                    }
+                }
+            }
+            Stmt result = IRMutator::visit(loop);
+            while (lets.size() > 0) {
+                auto let = lets.top();
+                result = LetStmt::make(let.first, let.second, result);
+                lets.pop();
+            }
+            return result;
+        }
+
         if (!ends_with(loop->name, ".__bank_id_x")) return IRMutator::visit(loop);
- 
-        const string kernel_name = "kernel_" + std::to_string(kernel_idx++); // TODO : to be changed
+
+        kernel_idx++;
+        string kernel_name = get_kernel_name();
 
         InspectResourceAllocation inspect;
         loop->accept(&inspect);
+        shared_memory_buffer = std::move(inspect.shared_memory_buffer);
 
         Stmt stmt_alloc_load = call_extern_and_assert("halide_upmem_alloc_load", { inspect.get_all_banks(), kernel_name });
 
         vector<Stmt> stmt_copies;
+        vector<Stmt> stmt_reduction;
         auto bounds = boxes_touched(inspect.thread_loop);
         for (auto it = bounds.begin(); it != bounds.end(); it++) {
-            auto box = it->second;
+            if (shared_memory_buffer.find(it->first) != shared_memory_buffer.end())
+                continue;
             vector<Expr> offset, sizes;
-            for (Interval b: box.bounds) {
+            for (Interval b: it->second.bounds) {
                 offset.push_back(b.min);
                 sizes.push_back(b.max - b.min + 1);
             }
             vector<Expr> args = { Variable::make(Int(32), "dpu_idx"), Variable::make(type_of<struct halide_buffer_t *>(), it->first + ".buffer") };
             args.insert(args.end(), offset.begin(), offset.end());
             args.insert(args.end(), sizes.begin(), sizes.end());
-            if (inspect.shared_memory_buffer.find(it->first) == inspect.shared_memory_buffer.end())
-                stmt_copies.push_back(Evaluate::make(Call::make(Int(32), "halide_upmem_dpu_copy_to", args, Call::Extern)));
+            stmt_copies.push_back(Evaluate::make(Call::make(Int(32), "halide_upmem_dpu_copy_to", args, Call::Extern)));
+            if (reduction_scope.contains(it->first)) {
+                stmt_reduction.push_back(Evaluate::make(Call::make(Int(32), "halide_upmem_dpu_copy_from", args, Call::Extern)));
+            }
         }
-        ThreadLoopMutator mutator(LetStmt::make("dpu_idx", inspect.bank, Block::make(stmt_copies)));
-        Stmt stmt_copy_to = mutator.mutate(loop);
+        
+        Stmt stmt_copy_to = ThreadLoopMutator(LetStmt::make("dpu_idx", inspect.bank, Block::make(stmt_copies))).mutate(loop);
+        Stmt stmt_copy_from = ThreadLoopMutator(LetStmt::make("dpu_idx", inspect.bank, Block::make(stmt_reduction))).mutate(loop);
 
         Stmt stmt_free = call_extern_and_assert("halide_upmem_free", { kernel_name });
 
-        // Stmt for_copy_reduction = For::make(loop->name + ".__reduction", Expr(loop->min), Expr(loop->extent), loop->for_type, DeviceAPI::None, for_copy_allocation);
+        bounds_scope.push(kernel_name, bounds);
+        auto result = IRMutator::visit(loop);
+        bounds_scope.pop(kernel_name);
 
-        
-        // I want to duplicate outermost PIM Bank type loop, and remove all other expressions but only remain store/load expressions and loop, if, and let depends to indexing store/load, and convert each store/load into device_copy.
-
-            // TODO : add ALLOC & LOAD
-            // TODO : add DISTRIBUTE
-            // TODO : add REDUCE
-            // TODO : add FREE
-        return Block::make({ stmt_alloc_load, stmt_copy_to, IRMutator::visit(loop), stmt_free });
+        return Block::make({ stmt_alloc_load, stmt_copy_to, result, stmt_copy_from, stmt_free });
     }
 };
 
